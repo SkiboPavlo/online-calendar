b7c8065a242f10d526bb2db36bb66dc8bae41ffb
SkiboPavlo <skibo.pavlo@gmail.com>
2018-08-09 12:36:44 +0300

    removed the users ability to register by e-mail and password

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,28 +1,18 @@
 class CalendarsController < ApplicationController
-  before_action :set_calendar, only: [:show, :edit, :update, :destroy]
+  before_action :set_calendar, only: %i[show edit update destroy]
 
-  # GET /calendars
-  # GET /calendars.json
   def index
     @calendars = Calendar.all
   end
 
-  # GET /calendars/1
-  # GET /calendars/1.json
-  def show
-  end
+  def show; end
 
-  # GET /calendars/new
   def new
     @calendar = Calendar.new
   end
 
-  # GET /calendars/1/edit
-  def edit
-  end
+  def edit; end
 
-  # POST /calendars
-  # POST /calendars.json
   def create
     @calendar = Calendar.new(calendar_params)
 
@@ -37,8 +27,6 @@ class CalendarsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /calendars/1
-  # PATCH/PUT /calendars/1.json
   def update
     respond_to do |format|
       if @calendar.update(calendar_params)
@@ -51,8 +39,6 @@ class CalendarsController < ApplicationController
     end
   end
 
-  # DELETE /calendars/1
-  # DELETE /calendars/1.json
   def destroy
     @calendar.destroy
     respond_to do |format|
@@ -62,13 +48,12 @@ class CalendarsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_calendar
-      @calendar = Calendar.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def calendar_params
-      params.require(:calendar).permit(:status)
-    end
+  def set_calendar
+    @calendar = Calendar.find(params[:id])
+  end
+
+  def calendar_params
+    params.require(:calendar).permit(:status)
+  end
 end
